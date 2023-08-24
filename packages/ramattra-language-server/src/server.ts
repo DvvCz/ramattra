ae63d75099dc453f6397d5cc9098c4767840c3ec
DvvCz <86097860+DvvCz@users.noreply.github.com>
2023-08-23 22:53:15 -0700

    Add hover provider

--- packages/ramattra-language-server/src/server.ts
+++ packages/ramattra-language-server/src/server.ts
@@ -15,7 +15,8 @@ export const onInit = (connection: Connection) => {
 				},
 				completionProvider: {
 					resolveProvider: true
-				}
+				},
+				hoverProvider: true
 			}
 		};
 	});
@@ -49,12 +50,84 @@ export const onInit = (connection: Connection) => {
 
 	documents.listen(connection);
 
-	connection.onCompletion(p => {
+	connection.onHover(p => {
 		const doc = documents.get(p.textDocument.uri);
 
 		if (!doc)
-			return [];
+			return;
 
+		const text = doc.getText();
+		const offset = doc.offsetAt(p.position);
+
+		if (text.charAt(offset).match(/[^\w]/))
+			return null;
+
+		let start = 0;
+		for (let i = offset; i > 0; i--) {
+			const char = text.charAt(i);
+			if (char.match(/[^\w]/)) {
+				start = i + 1;
+				break;
+			}
+		}
+
+		let end = text.length;
+		for (let i = offset; i < end; i++) {
+			const char = text.charAt(i);
+			if (char.match(/[^\w]/)) {
+				end = i;
+				break;
+			}
+		}
+
+		const word = text.slice(start, end);
+
+		if (FUNCTIONS[word]) {
+			const fn = FUNCTIONS[word]!;
+
+			return {
+				contents: {
+					kind: "markdown",
+					value: dedent`
+						${word}(${fn?.args.map(x => x.default ? `${x.type} = ${x.default}` : x.type).join(", ")})
+
+						**Overwatch Function**: \`${fn.ow}\`
+						[workshop.codes](${WORKSHOP_CODES_HREF}${fn.ow.replaceAll(" ", "+").toLowerCase()})
+					`
+				},
+			}
+		} else if (CONSTANTS[word]) {
+			const constant = CONSTANTS[word];
+			return {
+				contents: {
+					kind: "markdown",
+					value: dedent`
+						${word}: ${constant.type}
+
+						**Overwatch Value**: \`${constant.ow}\`
+					`
+				},
+			}
+		} else if (EVENTS[word]) {
+			const event = EVENTS[word];
+			return {
+				contents: {
+					kind: "markdown",
+					value: dedent`
+						event ${word}(${event.args.map(a => a.type).join(", ")})
+
+						**Overwatch Event**: \`${event.ow}\`
+						[workshop.codes](${WORKSHOP_CODES_HREF}${event.ow.replaceAll(" ", "+").toLowerCase()})
+
+						### Arguments
+						%S
+					`.replace("%S", event.args.map(a => `* **${a.ow}**: \`${a.type}\``).join("\n"))
+				},
+			}
+		}
+	});
+
+	connection.onCompletion(_p => {
 		const funcs = Object.keys(FUNCTIONS).map(name => {
 			return {
 				label: name,
